27fd7c8cfb175992795dc26accc3b48a27141fc6
Ryan <ryanjsims@email.arizona.edu>
2018-10-28 11:39:14 -0700

    Added component class

--- src/Component.h
+++ src/Component.h
@@ -1,6 +1,16 @@
 #ifndef __COMPONENT_H
 #define __COMPONENT_H
+#include <vector>
+#include <string>
 
-
+class Component{
+	protected:
+		int id;
+		std::vector<std::string> inputs;
+		std::vector<std::string> outputs;
+	public:
+		virtual std::string toString() = 0;
+		virtual std::string getID() = 0;
+}
 
 #endif
