0f1ec56bc48efe1bfe8dce9f7eb36e79624baf9a
Ryan <ryanjsims@email.arizona.edu>
2018-11-03 10:52:57 -0700

    Added skeleton to Comparator

--- src/components/Comparator.h
+++ src/components/Comparator.h
@@ -3,7 +3,23 @@
 #include "Component.h"
 
 class comp: public Component {
-	
+	private:
+		static int number;
+		
+	public:
+		//comp #(.DATAWIDTH(8)) c0 (in1, in2, gt, lt, eq)
+		comp(Netlist* netlist, std::string line){
+			this->netlist = netlist;
+			this->id = number;
+			number++;
+			if(line.find("<") != std::string::npos){
+				
+			} else if(line.find("==") != std::string::npos){
+
+			} else if(line.find(">") != std::string::npos){
+
+			}
+		}
 };
 
 #endif
