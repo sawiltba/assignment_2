7d8b5a166bfa0f5b946ca49353e09c2e5c45787a
Ryan <ryanjsims@email.arizona.edu>
2018-11-03 12:33:12 -0700

    Added Comparator implementation

--- src/components/Comparator.h
+++ src/components/Comparator.h
@@ -49,7 +49,18 @@ class comp: public Component {
 			bool sign = this->isSigned();
 			//[S]COMP #(.DATAWIDTH(<width>)) c# (a, b, gt, lt, eq);\n
 			size_t len = 36
-				+ this->componentName
+				+ this->IOStrLen()
+				+ this->getID().length()
+				+ std::to_string(this->getWidth()).length();
+			toReturn.resize(len);
+			char* str = (char*)malloc(len + 1);
+			sprintf(str, "%s #(.DATAWIDTH(%d)) %s (%s, %s, %s, %s, %s);\n",
+					this->componentName.c_str(), this->getWidth(),
+					this->getID().c_str(), inputs[0].c_str(), inputs[1].c_str(),
+					outputs[0].c_str(), outputs[1].c_str(), outputs[2].c_str());
+			toReturn.assign(str);
+			free(str);
+			return (sign ? "S" : "") + toReturn;
 		}
 };
 
