c1c9bb32795fb3fa4d6e4bfd144c29d0c333cbda
Matthew Motooka <mmotooka@email.arizona.edu>
2018-12-01 12:29:49 -0700

    ListL first commit

---
+++ src/ListL.cpp
@@ -0,0 +1,23 @@
+#include "ListL.h"
+
+int *ListL(Netlist netlist, int Latency)
+{
+	int a[3] = { 1 }; // Initialize vector a so all entries have value of 1
+	// Start times of all vectors already computed
+	int i = 1; // Set time step equal to 1
+
+	// Define candidate vectors
+	std::vector<Component>ALUCandidates;
+	std::vector<Component>LogCandidates;
+	std::vector<Component>MulCandidates;
+	int atemp[3] = { 1 }; // Define a counter
+
+	// Determine candidate operations
+
+	// Compute the slacks
+	// 
+
+
+
+	return a;
+}
---
+++ src/ListL.h
@@ -0,0 +1,10 @@
+#ifndef __LIST_L
+#define __LIST_L
+
+#include<vector>
+
+#include "Netlist.h"
+
+int *ListL(Netlist netlist, int Latency);
+
+#endif
