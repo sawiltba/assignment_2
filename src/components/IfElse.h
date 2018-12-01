c5e6dc7d9771e1bf75f9c40d28eee041e1d83092
Kyle-Cerniglia <cernigliakyle@gmail.com>
2018-12-01 14:17:43 -0700

    added ifelse toString

--- src/components/IfElse.h
+++ src/components/IfElse.h
@@ -15,8 +15,51 @@ class ifelse: public Component {
 			this->netlist = netlist;
 			this->idName = "ifelse";
 			this->componentName = "IFELSE";
+			this->latency = 1;
 			id = number;
 			number++;
+		}
+
+        int* ListRIfElse(){
+            int aIfElse[3];
+            int aIf[3] = ListR(if_branch);
+            int aElse[3] = ListR(else_branch);
+
+            if(aIf[0] > aElse[0]){
+                aIfElse[0] = aIf[0];
+            }
+            else{
+                aIfElse[0] = aElse[0];
+            }
+
+            if(aIf[1] > aElse[1]){
+                aIfElse[1] = aIf[1];
+            }
+            else{
+                aIfElse[1] = aElse[1];
+            }
+
+            if(aIf[2] > aElse[2]){
+                aIfElse[2] = aIf[2];
+            }
+            else{
+                aIfElse[2] = aElse[2];
+            }
+
+            return aIfElse;
+        }
+
+		int latencyIfElse(){
+            int lat = this->latency;
+            int lat_if = alap(if_branch);
+            int lat_else = alap(else_branch);
+            if(lat_if > lat_else){
+                lat += lat_if;
+            }
+            else{
+                lat += lat_else;
+            }
+            return lat;
 		}
 
 		void calcIOsIfElse(){
@@ -50,19 +93,30 @@ class ifelse: public Component {
 
 		std::string toString() override{
 			std::string toReturn;
-			//MUX2x1 #(.DATAWIDTH(<width>)) <id> (a, b, sel, sum);\n
-			size_t len = 34
-				+ this->IOStrLen()
-				+ this->getID().length()
-				+ std::to_string(this->getWidth()).length();
-			toReturn.resize(len);
-			char* str = (char*)malloc(len + 1);
-			sprintf(str, "MUX2x1 #(.DATAWIDTH(%d)) %s (%s, %s, %s, %s);\n",
-					this->getWidth(), this->getID().c_str(),
-					inputs[1].c_str(), inputs[2].c_str(), inputs[0].c_str(),
-					outputs[0].c_str());
-			toReturn.assign(str);
-			free(str);
+			int loop = 0;
+			//if ('condition')
+			toReturn += "if ( ";
+			toReturn += this->condition;
+			toReturn += " )\n";
+			toReturn += "begin\n";
+
+            while(loop < if_branch.operations.size()){
+                //if_branch.getComponents().at(loop)->getComponentName();
+                toReturn += if_branch.getComponents().at(loop)->toString();
+                toReturn += "\n";
+                loop++;
+            }
+            loop = 0;
+            toReturn += "end\n";
+
+            while(loop < else_branch.operations.size()){
+                toReturn += else_branch.getComponents().at(loop)->toString();
+                toReturn += "\n";
+                loop++;
+            }
+            loop = 0;
+            toReturn += "end\n";
+
 			return toReturn;
 		}
 };
