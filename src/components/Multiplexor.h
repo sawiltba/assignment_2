4277360b3aa73c3500db0cf42e02da2fb1d8f1aa
Matthew Motooka <mmotooka@email.arizona.edu>
2018-12-07 21:09:33 -0700

    printString -> toString

--- src/components/Multiplexor.h
+++ src/components/Multiplexor.h
@@ -34,24 +34,24 @@ class mux: public Component {
 		}
 
 		std::string toString() override{
-			std::string toReturn;
-			//MUX2x1 #(.DATAWIDTH(<width>)) <id> (a, b, sel, sum);\n
-			size_t len = 34 
-				+ this->IOStrLen()
-				+ this->getID().length()
-				+ std::to_string(this->getWidth()).length();
-			toReturn.resize(len);
-			char* str = (char*)malloc(len + 1);
-			sprintf(str, "MUX2x1 #(.DATAWIDTH(%d)) %s (%s, %s, %s, %s);\n",
-					this->getWidth(), this->getID().c_str(), 
-					inputs[1].c_str(), inputs[2].c_str(), inputs[0].c_str(),
-					outputs[0].c_str());
-			toReturn.assign(str);
-			free(str);
-			return toReturn;
-		}
+		//	std::string toReturn;
+		//	//MUX2x1 #(.DATAWIDTH(<width>)) <id> (a, b, sel, sum);\n
+		//	size_t len = 34 
+		//		+ this->IOStrLen()
+		//		+ this->getID().length()
+		//		+ std::to_string(this->getWidth()).length();
+		//	toReturn.resize(len);
+		//	char* str = (char*)malloc(len + 1);
+		//	sprintf(str, "MUX2x1 #(.DATAWIDTH(%d)) %s (%s, %s, %s, %s);\n",
+		//			this->getWidth(), this->getID().c_str(), 
+		//			inputs[1].c_str(), inputs[2].c_str(), inputs[0].c_str(),
+		//			outputs[0].c_str());
+		//	toReturn.assign(str);
+		//	free(str);
+		//	return toReturn;
+		//}
 
-		std::string printString() {
+		//std::string printString() {
 			std::string toPrint;
 			std::vector<std::string> outputs = this->getOutputs();
 			std::vector<std::string> inputs = this->getInputs();
--- src/components/Subtractor.h
+++ src/components/Subtractor.h
@@ -18,26 +18,26 @@ class sub: public Component {
 		}
 
 		std::string toString(){
-			std::string toReturn;
-			bool sign = this->isSigned();
-			//[S]<cName> #(.DATAWIDTH(<width>)) <id> (a, b, sum);\n
-			size_t len = 26
-				+ this->componentName.length() 
-				+ this->IOStrLen()
-				+ this->getID().length()
-				+ std::to_string(this->getWidth()).length();
-			toReturn.resize(len);
-			char* str = (char*)malloc(len + 1);
-			sprintf(str, "%s #(.DATAWIDTH(%d)) %s (%s, %s, %s);\n",
-					this->componentName.c_str(), this->getWidth(), 
-					this->getID().c_str(), inputs[0].c_str(), inputs[1].c_str(),
-					outputs[0].c_str());
-			toReturn.assign(str);
-			free(str);
-			return (sign ? "S" : "") + toReturn;
-		}
+		//	std::string toReturn;
+		//	bool sign = this->isSigned();
+		//	//[S]<cName> #(.DATAWIDTH(<width>)) <id> (a, b, sum);\n
+		//	size_t len = 26
+		//		+ this->componentName.length() 
+		//		+ this->IOStrLen()
+		//		+ this->getID().length()
+		//		+ std::to_string(this->getWidth()).length();
+		//	toReturn.resize(len);
+		//	char* str = (char*)malloc(len + 1);
+		//	sprintf(str, "%s #(.DATAWIDTH(%d)) %s (%s, %s, %s);\n",
+		//			this->componentName.c_str(), this->getWidth(), 
+		//			this->getID().c_str(), inputs[0].c_str(), inputs[1].c_str(),
+		//			outputs[0].c_str());
+		//	toReturn.assign(str);
+		//	free(str);
+		//	return (sign ? "S" : "") + toReturn;
+		//}
 
-		std::string printString() {
+		//std::string printString() {
 			std::string toPrint;
 			std::vector<std::string> outputs = this->getOutputs();
 			std::vector<std::string> inputs = this->getInputs();
